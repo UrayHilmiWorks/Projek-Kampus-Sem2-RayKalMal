f20083901a512ce405972659ca005ba3ffb12213
UrayHilmiWorks <164954255+UrayHilmiWorks@users.noreply.github.com>
2024-06-24 12:56:30 +0700

    Delete newFile.js

--- newFile.js
+++
@@ -1,6 +0,0 @@
-const { searchBtn, searchBar } = require("./main");
-
-searchBtn.addEventListener("click", () => {
-  searchBtn.classList.toggle("fa-times");
-  searchBar.classList.toggle("active");
-});
